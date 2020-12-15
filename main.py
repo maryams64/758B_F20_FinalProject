87648b50843fde8c7b5a69194a4e8398d8580e62
maryams64 <maryam.soomro129@gmail.com>
2020-12-14 20:56:28 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pandas as pd
 from preprocessing.preprocessing import cleandata
 from preprocessing.vectorization import get_counts, del_words, create_vocab, encode_sentences
+from preprocessing.ReviewDataset import ReviewDataset
+from model.MultiInputModel import MultiInputModel
 
 df = pd.read_json('/data/Digital_music_5.json', lines = True)
 
@@ -17,3 +19,27 @@ user_words = create_vocab(user_counts)
 
 df1['encoded_item'] = df1['reviewText_item'].apply(lambda x: np.array(encode_sentence(x, item_words[1])))
 df1['encoded_user']=df1['reviewText_user'].apply(lambda x: np.array(encode_sentence(x, user_words[1])))
+
+X=list(df1['encoded_item'])
+X2=list(df1['encoded_user'])
+y = list(df1['overall_avg'])
+
+X_item_train, X_item_valid, y_item_train, y_item_valid = train_test_split(X, y, test_size=0.2)
+X_user_train, X_user_valid, y_user_train, y_user_valid = train_test_split(X2, y, test_size=0.2)
+
+train_item_ds = ReviewsDataset(X_item_train, y_item_train)
+valid_item_ds = ReviewsDataset(X_item_valid, y_item_valid)
+
+train_user_ds = ReviewsDataset(X_user_train, y_user_train)
+valid_user_ds = ReviewsDataset(X_user_valid, y_user_valid)
+
+batch_size = 5000
+item_vocab = len(item_words[0])
+user_vocab = len(user_words[0])
+item_train_dl = DataLoader(train_item_ds, batch_size=batch_size)
+item_valid_dl = DataLoader(valid_item_ds, batch_size=batch_size)
+
+user_train_dl = DataLoader(train_user_ds, batch_size=batch_size)
+user_valid_dl = DataLoader(valid_user_ds, batch_size=batch_size)
+
+MultiInputModel(item_train_dl,item_valid_dl,item_vocab,user_train_dl,user_valid_dl,user_vocab,100,70)
