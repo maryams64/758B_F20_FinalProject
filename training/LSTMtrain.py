854c0bb43520a8e8d33c746948dc1c3c3dbacf91
maryams64 <maryam.soomro129@gmail.com>
2020-12-17 00:31:01 -0500

    Update LSTMtrain.py

--- training/LSTMtrain.py
+++ training/LSTMtrain.py
@@ -7,7 +7,9 @@ import torch.nn.functional as F
 import torch.optim as optim
 from torch.autograd import Variable
 from torch.utils.data import Dataset, DataLoader, TensorDataset
+import matplotlib.pyplot as plt
 
+valid_scores = []
 #defining the functions which will find the best LSTM Model
 def train_LSTMmodel(model, dataloader, val, num, epochs=10, lr=0.001):
     
@@ -28,13 +30,20 @@ def train_LSTMmodel(model, dataloader, val, num, epochs=10, lr=0.001):
             loss.backward()
             optimizer.step()
             sum_loss += loss.item()*Y.shape[0]
-            total += Y.shape[0]    
+            total += Y.shape[0]
         val_loss, val_acc, val_rmse = validation_LSTMmetrics(model, val)
+        valid_scores.append(i,val_loss, val_acc, val_rmse)
         if i % 5 == 1:
             #print("epoch: %.1f: train loss %.3f, val loss %.3f, val accuracy %.3f, and val rmse %.3f" % (i, sum_loss/total, val_loss, val_acc, val_rmse))
             modelSave = savebestmodel(val_acc, max, metrics)
             if modelSave:
               torch.save(model.state_dict(), 'model/model'+str(num)+'.pt')
+    valid_data = pd.Datagrame(valid_scores,columns=['Epoch','Valid_Loss','Valid_Accuracy','Valid_RMSE'])
+    valid_data.set_index('Epoch',inplace=True)
+    plt.figure(figsize=(10, 10))
+    plt.plot(valid_data[0], valid_data[1:])
+    plt.savefig("model_scores.png")
+    
     return Y_pred
     
 def validation_LSTMmetrics (model, val):
