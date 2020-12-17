af73bd87233d88291f0577663fe400a8c5767380
maryams64 <maryam.soomro129@gmail.com>
2020-12-16 21:39:03 -0500

    Update LSTMtrain.py

--- training/LSTMtrain.py
+++ training/LSTMtrain.py
@@ -46,7 +46,7 @@ def validation_LSTMmetrics (model, val):
     for X, Y, h in val:
         X = X.long()
         Y = Y.long()
-        Y_hat = model(X, h)
+        Y_hat = model(X, l)
         loss = F.cross_entropy(Y_hat, Y)
         pred = torch.max(Y_hat, 1)[1]
         correct += (pred == Y).float().sum()
@@ -72,4 +72,3 @@ def savebestmodel(val_acc, max, metrics):
     modelSave = True
     max.append(metrics[0])
   return modelSave
-
