00bee36d6c0042655681fee27852fc3d00e245c2
maryams64 <maryam.soomro129@gmail.com>
2020-12-16 21:37:34 -0500

    Update main.py

--- main.py
+++ main.py
@@ -14,14 +14,10 @@ from torch.utils.data import Dataset, DataLoader, TensorDataset
 from sklearn.model_selection import train_test_split
 
 print("Reading data....")
-
 df = pd.read_json('data/Digital_Music_5.json', lines = True)
 
 print("Cleaning data....")
 df1 = cleandata(df)
-
-df1['reviewText_item'] = df1['reviewText_item'].astype(str)
-df1['reviewText_user'] = df1['reviewText_user'].astype(str)
 
 print("Vectorizing data....")
 item_counts = get_counts(df1, 'reviewText_item')
@@ -37,12 +33,9 @@ df1['encoded_item'] = df1['reviewText_item'].apply(lambda x: np.array(encode_sen
 df1['encoded_user']=df1['reviewText_user'].apply(lambda x: np.array(encode_sentence(x, user_words[1])))
 
 print("Creating training and validation datasets....")
-
-X = list(df1['encoded_item'])
-X2 = list(df1['encoded_user'])
+X=list(df1['encoded_item'])
+X2=list(df1['encoded_user'])
 y = list(df1['overall_avg'])
-
-
 
 X_item_train, X_item_valid, y_item_train, y_item_valid = train_test_split(X, y, test_size=0.2)
 X_user_train, X_user_valid, y_user_train, y_user_valid = train_test_split(X2, y, test_size=0.2)
@@ -61,7 +54,6 @@ item_valid_dl = DataLoader(valid_item_ds, batch_size=batch_size)
 
 user_train_dl = DataLoader(train_user_ds, batch_size=batch_size)
 user_valid_dl = DataLoader(valid_user_ds, batch_size=batch_size)
-
 
 print("Running MultiInputModel....")
 MultiInputModel(item_train_dl,item_valid_dl,item_vocab,user_train_dl,user_valid_dl,user_vocab,100,70,y)
