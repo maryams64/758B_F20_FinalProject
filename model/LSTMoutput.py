37d2ca6ca2be2fe89b5e6cd54917be908577d945
maryams64 <maryam.soomro129@gmail.com>
2020-12-17 19:57:42 -0500

    Update LSTMoutput.py

--- model/LSTMoutput.py
+++ model/LSTMoutput.py
@@ -13,8 +13,8 @@ from torch.utils.data import Dataset, DataLoader, TensorDataset
 ##Functions that will seek the best model + load it and get final output
 def getLSTMModel(item_train_dl,item_valid_dl,item_vocab,user_train_dl,user_valid_dl,user_vocab,embedding_dim,hidden_dim):
   print('Finding the best LSTM Models....')
-  lstm_item_model = LSTM_fixed_len2(item_vocab, embedding_dim, hidden_dim)
-  lstm_user_model = LSTM_fixed_len2(user_vocab, embedding_dim, hidden_dim)
+  lstm_item_model = LSTM(item_vocab, embedding_dim, hidden_dim)
+  lstm_user_model = LSTM(user_vocab, embedding_dim, hidden_dim)
 
   lstm_item_output = train_LSTMmodel(lstm_item_model, item_train_dl, item_valid_dl, 1, epochs=30, lr=0.01)
   lstm_user_output = train_LSTMmodel(lstm_user_model, user_train_dl, user_valid_dl, 2, epochs=30, lr=0.01)
@@ -22,8 +22,8 @@ def getLSTMModel(item_train_dl,item_valid_dl,item_vocab,user_train_dl,user_valid
   item_model_PATH = 'model/model1.pt'
   user_model_PATH = 'model/model2.pt'
 
-  item_LSTMmodel = LSTM_fixed_len2(item_vocab, embedding_dim, hidden_dim)
-  user_LSTMmodel = LSTM_fixed_len2(user_vocab, embedding_dim, hidden_dim)
+  item_LSTMmodel = LSTM(item_vocab, embedding_dim, hidden_dim)
+  user_LSTMmodel = LSTM(user_vocab, embedding_dim, hidden_dim)
 
   item_LSTMmodel.load_state_dict(torch.load(item_model_PATH))
   user_LSTMmodel.load_state_dict(torch.load(user_model_PATH))
